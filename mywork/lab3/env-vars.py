1abda92809bcd111e9935cf014014420085e153c
divyakuruvilla <yaq7fm@virginia.edu>
2024-02-22 11:41:52 -0500

    finished python script

--- mywork/lab3/env-vars.py
+++ mywork/lab3/env-vars.py
@@ -2,17 +2,21 @@
 
 import os
 
+# Prompt User for Input
+# Store in 4 Variables 
 FAV_SPORT = input('What is your favorite sport? ')
 UVA_YEAR = input('What is your UVA year standing? ')
 FAV_FOOD = input('What is your favorite food? ' )
+FAV_GENRE = input('What is your favorite genre of music? ')
 
-# Fetch the variables 
+# Set the Environment Variables 
 os.environ["FAV_SPORT"] = FAV_SPORT
 os.environ["UVA_YEAR"] = UVA_YEAR
 os.environ["FAV_FOOD"] = FAV_FOOD
+os.environ["FAV_GENRE"] = FAV_GENRE 
 
-# Print the Variables 
-print(FAV_SPORT)
-print(UVA_YEAR)
-print(FAV_FOOD)
-
+# Fetch and Print the Envrionment Variables 
+print(os.getenv("FAV_SPORT"))
+print(os.getenv("UVA_YEAR"))
+print(os.getenv("FAV_FOOD"))
+print(os.getenv("FAV_GENRE"))
